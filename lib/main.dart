1a521f0d75d847079299f6f7444ebadd7ef4c7c4
FAKHRIDDIN <faxriddinabdumajidov4606@gmail.com>
2023-02-23 17:23:33 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:hive_lesson/Model/person_model.dart';
+import 'package:hive_lesson/Model/universe_model.dart';
+
+import 'view/home_page.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
+  Hive.registerAdapter(UniverResponseAdapter());
+  Hive.registerAdapter(UniverAdapter());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
